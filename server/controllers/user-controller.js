743298b84862673480e7208aa16c1804b51a5ec4
JQueen <queen_jenf@yahoo.com>
2020-12-11 18:56:59 -0800

    server files

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,10 +1,8 @@
-// import user model
 const { User } = require('../models');
-// import sign token function from auth
 const { signToken } = require('../utils/auth');
 
 module.exports = {
-  // get a single user by either their id or their username
+
   async getSingleUser({ user = null, params }, res) {
     const foundUser = await User.findOne({
       $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
@@ -16,7 +14,7 @@ module.exports = {
 
     res.json(foundUser);
   },
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+
   async createUser({ body }, res) {
     const user = await User.create(body);
 
@@ -26,8 +24,7 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
-  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-  // {body} is destructured req.body
+
   async login({ body }, res) {
     const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
     if (!user) {
@@ -42,8 +39,7 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-  // user comes from `req.user` created in the auth middleware function
+
   async saveBook({ user, body }, res) {
     console.log(user);
     try {
@@ -58,7 +54,7 @@ module.exports = {
       return res.status(400).json(err);
     }
   },
-  // remove a book from `savedBooks`
+
   async deleteBook({ user, params }, res) {
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id },
